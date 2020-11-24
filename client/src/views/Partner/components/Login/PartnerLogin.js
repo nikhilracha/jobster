555df520defb85db8a0d7e8215400b7131fe4477
Nikhil Racha <rachanikhil41@gmail.com>
2020-11-23 20:03:52 -0500

    Fixed a bug in partner Login and dashboard

--- client/src/views/Partner/components/Login/PartnerLogin.js
+++ client/src/views/Partner/components/Login/PartnerLogin.js
@@ -24,8 +24,13 @@ import { PartnerLoginSchema } from '../../../../validation/Validations';
 import { compose } from "recompose";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { asyncPartnerLogin } from "../../../../actions/authActions";
+import { asyncPartnerLogin, setCurrentPartner } from "../../../../actions/authActions";
 import { useHistory } from 'react-router-dom';
+
+
+import setAuthToken from "../../../../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import store from "../../../../store";
 
 
 
@@ -88,6 +93,32 @@ function PartnerLogin(props) {
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
+
+    React.useEffect(() => {
+        // Check for token
+        if (localStorage.pt_jwtToken) {
+            // Set auth token header auth
+            console.log("in effect", localStorage.pt_jwtToken);
+            setAuthToken(localStorage.pt_jwtToken);
+            // Decode token and get user info and exp
+            const decoded = jwt_decode(localStorage.pt_jwtToken);
+            console.log("in effect decoded", decoded);
+            // Set user and isAuthenticated
+            store.dispatch(setCurrentPartner(decoded));
+
+            // Check for expired token
+            const currentTime = Date.now() / 1000;
+            if (decoded.exp < currentTime) {
+                //console.log("App.js is running inside if and if")
+                // Logout user
+                //store.dispatch(logoutUser());
+                // TODO: Clear current Profile
+                console.log("Session Expired")
+                // Redirect to login
+                // window.location.href = "/login";
+            }
+        }
+    }, [])
 
     React.useEffect(() => {
         console.log("props", props)
