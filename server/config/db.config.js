9cac8d8e651206596688dc6dcac8e09431e5a7c7
Nikhil Racha <rachanikhil41@gmail.com>
2020-11-23 19:26:17 -0500

    updated the DB config file

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 //local mysql db connection
 const dbConn = mysql.createConnection({
-    host: 'sql9.freesqldatabase.com',  //localhost
-    user: 'sql9375796',    //your username
-    password: 'wiNfw1aGyi',     //your password
-    database: 'sql9375796' //your database name
+    host: 'sql9.freemysqlhosting.net',  //localhost
+    user: 'sql9378231',    //your username
+    password: 're4w5hUWrq',     //your password
+    database: 'sql9378231' //your database name
 });
 dbConn.connect(function (err) {
     if (err) throw err;
