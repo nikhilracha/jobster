3d19f36f7561b337265ef6b3fa1ca6e622ad845a
ishaanchawla0001 <ishaanchawla0001@gmail.com>
2021-03-02 12:42:56 -0500

    testing git control

---
+++ client/src/components/Header-adv/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { AppBar, Toolbar, Grid, makeStyles, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: '#0e76a8',
+        
+    },
+    logoColor: {
+       color: '#fff'
+    }
+}))
+
+export default function Header() {
+
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static" className={classes.root}>
+            <Toolbar>
+                
+                    <Typography className={classes.logoColor} variant="h5">
+                        Jobster-Adv Mgmt.
+                    </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
