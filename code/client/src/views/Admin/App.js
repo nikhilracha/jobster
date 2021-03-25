7ba08e8d485d172b1f8f3dfd758d23918df3d112
Nikhil Racha <rachanikhil41@gmail.com>
2021-03-24 20:37:23 -0400

    fixed

--- code/client/src/views/Admin/App.js
+++ code/client/src/views/Admin/App.js
@@ -22,14 +22,14 @@ const theme = createMuiTheme({
       default: "#f4f5fd"
     },
   },
-  
-  props:{
-    MuiIconButton:{
-      disableRipple:true
+
+  props: {
+    MuiIconButton: {
+      disableRipple: true
     }
   },
-  form:{
-    
+  form: {
+
   }
 })
 
@@ -47,109 +47,108 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className={classes.appMain}>
-        <Header /> 
+        <Header />
         <Box
-        display="flex"
-        flexDirection="column"
-        height="100%"
-        justifyContent="center"
-        marginTop='100px'
-      >
-        <Container maxWidth="sm">
-          <Formik 
-            initialValues={{
-              email: '',
-              password: ''
-            }}
-            validationSchema={Yup.object().shape({
-              email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-              password: Yup.string().max(255).required('Password is required')
-            })}
+          display="flex"
+          flexDirection="column"
+          height="100%"
+          justifyContent="center"
+          marginTop='100px'
+        >
+          <Container maxWidth="sm">
+            <Formik
+              initialValues={{
+                email: '',
+                password: ''
+              }}
+              validationSchema={Yup.object().shape({
+                email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+                password: Yup.string().max(255).required('Password is required')
+              })}
               onSubmit={(data) => {
-                
+
                 console.log(data)
-                axios.post("http://localhost:5000/api/a-login", data, { timeout: 10000 }).then((res)=>{
-                console.log(res.data)
-                if (res.data.success)
-                {
-                  localStorage.setItem("a_jwtToken", JSON.stringify(res.data.token));
-                  history.push('/a_home')
-                }
+                axios.post("http://localhost:5000/api/a-login", data, { timeout: 10000 }).then((res) => {
+                  console.log(res.data)
+                  if (res.data.success) {
+                    localStorage.setItem("a_jwtToken", JSON.stringify(res.data.token));
+                    history.push('/a_home')
+                  }
                 })
-                .catch((err)=>{
-                  console.log(err)
+                  .catch((err) => {
+                    console.log(err)
                   })
-                }}
-          >
-            {({
-              errors,
-              handleBlur,
-              handleChange,
-              handleSubmit,
-              isSubmitting,
-              touched,
-              values
-            }) => (
-              <form onSubmit={handleSubmit} autoComplete="off">
-                <Box mb={3}>
-                  <Typography
-                    color="textPrimary"
-                    variant="h3"
-                  >
-                    Sign in
+              }}
+            >
+              {({
+                errors,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                isSubmitting,
+                touched,
+                values
+              }) => (
+                  <form onSubmit={handleSubmit} autoComplete="off">
+                    <Box mb={3}>
+                      <Typography
+                        color="textPrimary"
+                        variant="h3"
+                      >
+                        Sign in
                   </Typography>
-                  <Typography
-                    color="textSecondary"
-                    gutterBottom
-                    variant="body2"
-                  >
-                    Welcome to Advertisement Mangement sign in!
+                      <Typography
+                        color="textSecondary"
+                        gutterBottom
+                        variant="body2"
+                      >
+                        Welcome to Advertisement Mangement sign in!
                   </Typography>
-                </Box>
-                <TextField
-                  autoComplete = "off"
-                  error={Boolean(touched.email && errors.email)}
-                  fullWidth
-                  helperText={touched.email && errors.email}
-                  label="Email Address"
-                  margin="normal"
-                  name="email"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="email"
-                  value={values.email}
-                  variant="outlined"
-                />
-                <TextField
-                  autoComplete = "off"
-                  error={Boolean(touched.password && errors.password)}
-                  fullWidth
-                  helperText={touched.password && errors.password}
-                  label="Password"
-                  margin="normal"
-                  name="password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="password"
-                  value={values.password}
-                  variant="outlined"
-                />
-                <Box my={2}>
-                  <Button
-                    color='primary'
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                  >
-                    Sign in
+                    </Box>
+                    <TextField
+                      autoComplete="off"
+                      error={Boolean(touched.email && errors.email)}
+                      fullWidth
+                      helperText={touched.email && errors.email}
+                      label="Email Address"
+                      margin="normal"
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      type="email"
+                      value={values.email}
+                      variant="outlined"
+                    />
+                    <TextField
+                      autoComplete="off"
+                      error={Boolean(touched.password && errors.password)}
+                      fullWidth
+                      helperText={touched.password && errors.password}
+                      label="Password"
+                      margin="normal"
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      type="password"
+                      value={values.password}
+                      variant="outlined"
+                    />
+                    <Box my={2}>
+                      <Button
+                        color='primary'
+                        fullWidth
+                        size="large"
+                        type="submit"
+                        variant="contained"
+                      >
+                        Sign in
                   </Button>
-                </Box>
-              </form>
-            )}
-          </Formik>
-        </Container>
-      </Box>
+                    </Box>
+                  </form>
+                )}
+            </Formik>
+          </Container>
+        </Box>
       </div>
       <CssBaseline />
     </ThemeProvider>
